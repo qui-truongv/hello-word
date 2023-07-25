1746b6f305b1bda96d6d04f078975003469e3c36
qui-truongv <41338602+qui-truongv@users.noreply.github.com>
2023-07-25 13:45:46 +0700

    Push code lần đầu
    Đẩy code lần đầu lên github

---
+++ helloWord/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace helloWord.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
